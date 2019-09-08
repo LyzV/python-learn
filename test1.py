5b31b26c2826b80bc77627fe73048f27021223f7
vladimir lyz <lyzv@yandex.ru>
2019-09-08 22:53:01 +0300

    started

---
+++ test1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+print('Hello, Python3.5!')
+print('3.14')
+
+age = 26
+print('age consist from {0} quantity bits'.format(age.bit_length()))
+name = 'Swaroop'
+print('Возраст {0} -- {1} лет.'.format(name, age))
+print("Почему {0} забавляется с этим Python?".format(name))
+print('''Возраст {0} -- {1} лет.''')
+
+integer_var=1234
+integer_var+=1
+print (integer_var)
